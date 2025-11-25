d8475b690fd23a25345507e87436774379f83480
Thanasuk00 <thanauk4745@gmail.com>
2025-11-25 14:26:38 +0700

    chore-components

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,10 @@
-// src/components/Providers.tsx
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-// ❌ ลบ import { SSRProvider } ออก
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-       {/* ❌ ลบ <SSRProvider> ที่ครอบไว้ออก ให้เหลือแค่ children เพียวๆ */}
         {children}
     </SessionProvider>
   );
